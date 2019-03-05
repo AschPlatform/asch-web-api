c6b5cdb683103edb36fdc6a6a6daa7f856b0aa9e
kimziv <kimziv@qq.com>
2019-03-05 16:21:35 +0800

    clean files

--- src/providers/Provider.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '../type'
-export interface Provider {
-  get: (url: string, params: ObjectType) => Promise<object>
-  post: (url: string, params: ObjectType, headers?: ObjectType) => Promise<object>
-  // getContract: (url: string) => Promise<object>
-}
