c7ee3a1d8a59554cad6818675bd68c3cffb2ce8f
kimziv <kimziv@qq.com>
2019-04-09 16:20:12 +0800

    add comments for query apis

--- src/asch-api.ts
+++ src/asch-api.ts
@@ -12,16 +12,16 @@ export default class AschAPI extends API {
     this.aschWeb = aschWeb
   }
 
-
   /**
    * 
    * 以下接口为查询接口
    * 
-  */
+   */
 
 
   /**
-   * 
+   * 本地不加密直接登陆
+   * 备注：将密码以明文方式传入到后端，根据生成的地址去查询账户信息。不推荐在公网坏境使用！
    * @param secret 
    */
   public async open(secret: string): Promise<object>{
@@ -29,233 +29,539 @@ export default class AschAPI extends API {
   }
 
   /**
-   * 
-   * @param address 
+   * 本地加密后再登陆（推荐使用）
+   * @param publicKey 
    */
   public async open2(publicKey: string): Promise<object>{
     return this.post(URLS.accounts.open2,{publicKey:publicKey})
   }
 
+  /**
+   * 根据地址获取账户信息
+   * @param address 
+   */
   public async getAccountDetail(address: string): Promise<object>{
-    return this.get(URLS.v2.accounts.detail+'/'+address)
+    return this.get(URLS.v2.accounts.detail.replace(':address', address))
   }
 
+  /**
+   * 获取账户资产余额
+   * @param address 账户地址
+   */
   public async queryBalances(address: string): Promise<object>{
-    return this.get(URLS.v2.accounts.balances+'/'+address)
+    return this.get(URLS.v2.accounts.balances.replace(':address',address))
   }
 
+  /**
+   * 根据地址获取其投票列表
+   * @param address 账户地址
+   */
   public async queryVotedDelegates(address: string): Promise<object>{
-    return this.get(URLS.accounts.deletates_voted+'/'+address)
+    return this.get(URLS.accounts.deletates_voted, {address: address})
   }
 
+  /**
+   * 生成新账户
+   */
   public async generateAccount(): Promise<object>{
     return this.get(URLS.accounts.new)
   }
 
+  /**
+   * 获取当前链上账户总个数
+   */
   public async getAccountCount(): Promise<object>{
     return this.get(URLS.accounts.count)
   }
 
-  public async queryTransactions(orderBy: string, height: number|string, senderId:string, message:string, offset:number, limit:number): Promise<object>{
-    return this.get(URLS.v2.transactions.list)
+  /**
+   * 获取交易列表
+   * @param offset 偏移量
+   * @param limit 个数限制
+   * @param orderBy 排序方式
+   * @param height 交易所所在高度
+   * @param senderId 交易签名者地址
+   * @param message 交易备注
+   */
+  public async queryTransactions(offset:number, limit:number, orderBy: string, height: number|string, senderId:string, message:string): Promise<object>{
+    return this.get(URLS.v2.transactions.list,{
+      offset:offset,
+      limit:limit,
+      orderBy:orderBy,
+      height:height,
+      senderId:senderId,
+      message: message
+    })
   }
 
+  /**
+   *  根据交易id查看交易详情
+   * @param tid 
+   */
   public async getTransactionDetail(tid: string): Promise<object>{
-    return this.get(URLS.v2.transactions.detail+'/'+tid)
+    return this.get(URLS.v2.transactions.detail.replace(':tid',tid))
   }
 
+  /**
+   * 根据id查看未确认交易详情
+   * @param tid 交易id
+   */
   public async getUnconfirmedTransactionDetail(tid: string): Promise<object>{
-    return this.get(URLS.transactions.uncomfirmed_detail+'/'+tid)
+    return this.get(URLS.transactions.uncomfirmed_detail,{id:tid})
   }
 
+  /**
+   * 获取[全网所有]未确认的交易详情
+   * 接口说明：如果不加参数，则会获取全网所有未确认交易
+   * @param senderPublicKey 发送者公钥
+   * @param address 地址
+   */
   public async queryUnconfirmedTransactions(senderPublicKey: string='', address: string=''): Promise<object>{
-    return this.get(URLS.transactions.uncomfirmed+'/'+address)
+    return this.get(URLS.transactions.uncomfirmed,
+      {
+        senderPublicKey:senderPublicKey,
+        address:address
+      })
   }
 
-  public async queryTransfers(limit: number, offset:number, ownerId:string, currency:string): Promise<object>{
+  /**
+   * 获取转账记录
+   * @param limit 返回个数限制（默认20）
+   * @param offset 偏移量
+   * @param ownerId 发送/接收者ID
+   * @param currency 币种
+   */
+  public async queryTransfers(limit: number=20, offset:number, ownerId:string, currency:string): Promise<object>{
     return this.get(URLS.v2.transfers.list)
   }
 
-  public async getBlockDetail(idOrHeightOrHash: string): Promise<object>{
-    return this.get(URLS.blocks.detail+'/'+idOrHeightOrHash)
+  /**
+   *  根据区块ID获取指定区块的详情
+   * @param id 区块id
+   */
+  public async getBlockDetailById(id:string): Promise<object>{
+    return this.get(URLS.blocks.detail,{id:id})
   }
 
-  public async queryBlocks(limit: number, offset: number, orderBy: string): Promise<object>{
+  /**
+   *  根据区块高度获取指定区块的详情
+   * @param height 区块高度
+   */
+  public async getBlockDetailByHeight(height:string): Promise<object>{
+    return this.get(URLS.blocks.detail,{height:height})
+  }
+
+  /**
+   *  根据区块hash获取指定区块的详情
+   * @param hash 区块hash
+   */
+  public async getBlockDetailByHash(hash:string): Promise<object>{
+    return this.get(URLS.blocks.detail,{hash:hash})
+  }
+
+  /**
+   * 获取区块列表
+   * @param limit 个数限制
+   * @param offset 偏移量
+   * @param orderBy height:desc或者height:asc(目前提供的排序方法)
+   */
+  public async queryBlocks(limit: number=20, offset: number, orderBy: string): Promise<object>{
     return this.get(URLS.v2.blocks.list)
   }
 
-  public async getBlockDetailV2(idOrHeight: string): Promise<object>{
-    return this.get(URLS.v2.blocks.detail+'/'+idOrHeight)
+  /**
+   *  根据id或高度获取区块详情
+   * @param idOrHeight id或高度
+   */
+  public async getBlockDetailByIdOrHeight(idOrHeight: string): Promise<object>{
+    return this.get(URLS.v2.blocks.detail.replace(':idOrHeight',idOrHeight))
   }
 
+  /**
+   * 获取区块链高度
+   */
   public async getBlocksHeight(): Promise<object>{
     return this.get(URLS.blocks.height)
   }
 
+  /**
+   * 获取里程碑信息
+   */
   public async getMilestone(): Promise<object>{
     return this.get(URLS.blocks.milestone)
   }
 
+  /**
+   * 查看单个区块奖励
+   */
   public async getBlockReward(): Promise<object>{
     return this.get(URLS.blocks.reward)
   }
 
+  /**
+   * 获取XAS当前供应量
+   */
   public async getXASSupply(): Promise<object>{
     return this.get(URLS.blocks.supply)
   }
 
+  /**
+   * 获取区块链状态
+   */
   public async getBlockchainStatus(): Promise<object>{
     return this.get(URLS.blocks.status)
   }
 
-  public async getBlockFullDetail(idOrHeight: string): Promise<object>{
-    return this.get(URLS.blocks.full+'/'+idOrHeight)
+  /**
+   * 根据区块ID获取指定区块的交易信息
+   * @param id 区块id
+   */
+  public async getBlockFullDetailById(id: string): Promise<object>{
+    return this.get(URLS.blocks.full,{id:id})
   }
 
+  /**
+   * 根据区块高度获取指定区块的交易信息
+   * @param height 区块高度
+   */
+  public async getBlockFullDetailByHeight(height: string): Promise<object>{
+    return this.get(URLS.blocks.full,{height:height})
+  }
+
+  /**
+   * 获取受托人总个数
+   */
   public async getDelegatesCount(): Promise<object>{
     return this.get(URLS.delegates.count)
   }
 
+  /**
+   * 根据受托人名字查看哪些人为其投了票
+   * @param name 受托人名字
+   */
   public async getVotersOfDelegate(name: string): Promise<object>{
-    return this.get(URLS.delegates.voters+'/'+name)
+    return this.get(URLS.delegates.voters,{name:name})
   }
 
-  public async getDelegateDetail(publickeyOrName: string): Promise<object>{
-    return this.get(URLS.delegates.detail+'/'+publickeyOrName)
+  /**
+   * 根据公钥获取受托人详情
+   * @param publickey 受托人公钥
+   */
+  public async getDelegateDetailByPublickey(publickey: string): Promise<object>{
+    return this.get(URLS.delegates.detail,{publickey:publickey})
   }
 
+  /**
+   * 根据用户名获取受托人详情
+   * @param name 受托人用户名
+   */
+  public async getDelegateDetailByName(name: string): Promise<object>{
+    return this.get(URLS.delegates.detail,{name:name})
+  }
+
+  /**
+   * 获取受托人列表
+   * 接口说明：如果不加参数则会返回全网受托人列表
+   * @param limit 数量
+   * @param offset 偏移量，最小值：0
+   * @param orderBy 排序字段:排序规则，如:desc
+   */
   public async queryDelegates(limit: number, offset: number, orderBy: string): Promise<object>{
     return this.get(URLS.delegates.list)
   }
 
+  /**
+   * 受托人锻造状态查看
+   * @param publicKey 公钥
+   */
   public async getForgingStatusOfDelegate(publicKey: string): Promise<object>{
     return this.get(URLS.delegates.forging_status+'/'+publicKey)
   }
 
+  /**
+   * 获取当前节点连接的所有节点信息
+   * 备注：展示节点只是和本机有连接的节点，并不是全网所有的节点
+   * @param limit 数量限制
+   * @param offset 偏移量
+   */
   public async queryPeers(limit: number, offset: number): Promise<object>{
     return this.get(URLS.peers.list)
   }
 
+  /**
+   * 获取当前节点版本号等信息
+   */
   public async getPeerVersion(): Promise<object>{
     return this.get(URLS.peers.version)
   }
 
+  /**
+   * 获取指定ip节点信息
+   * @param ip 待查询节点ip
+   * @param port 待查询节点端口，1~65535
+   */
   public async getPeerDetail(ip: string, port: number): Promise<object>{
     return this.get(URLS.peers.detail)
   }
 
+  /**
+   * 查看当前节点区块链加载状态
+   */
   public async getLoaderStatus(): Promise<object>{
     return this.get(URLS.loder.status)
   }
 
+  /**
+   * 查看区块同步信息
+   */
   public async getLoaderStatusSync(): Promise<object>{
     return this.get(URLS.loder.status_sync)
   }
 
+  /**
+   * 获取提案列表
+   * @param limit 数量限制
+   * @param offset 偏移量
+   */
   public async queryProposals(limit: number, offset: number): Promise<object>{
     return this.get(URLS.v2.proposals.list)
   }
 
+  /**
+   * 根据提案id获取提案详情
+   * @param pid 提案id
+   */
   public async getProposalDetail(pid: string): Promise<object>{
     return this.get(URLS.v2.proposals.detail+'/'+pid)
   }
 
+  /**
+   * 获取网关列表
+   * @param limit 数量限制
+   * @param offset 偏移量
+   */
   public async queryGateways(limit: number, offset: number): Promise<object>{
     return this.get(URLS.v2.gateways.list)
   }
 
+  /**
+   * 获取指定网关的验证者
+   * @param gateway 网关名字
+   */
   public async queryValidatorsOfGateway(gateway: string): Promise<object>{
     return this.get(URLS.v2.gateways.validators+'/'+gateway)
   }
 
+  /**
+   * 获取支持的所有跨链币种
+   */
   public async queryAllCurrenciesOfGateways(): Promise<object>{
     return this.get(URLS.v2.gateways.currencies_all)
   }
 
+  /**
+   * 获取指定网关的支持币种
+   * @param gateway 网关名称
+   */
   public async queryCurrenciesOfGateway(gateway: string): Promise<object>{
-    return this.get(URLS.v2.gateways.account_one+'/'+gateway)
+    return this.get(URLS.v2.gateways.currencies_one+'/'+gateway)
   }
 
-  public async getAccountOfGateway(name: string, gateway: string): Promise<object>{
+  /**
+   * 获取指定网关的指定账户
+   * @param gateway 网关名字
+   * @param address 账户地址
+   */
+  public async getAccountOfGateway(gateway: string, address: string): Promise<object>{
     return this.get(URLS.v2.gateways.account_one)
   }
 
-  public async queryAccountsOfGateway(gateway: string): Promise<object>{
-    return this.get(URLS.v2.gateways.account_all+'/'+gateway)
+  /**
+   * 获取指定用户地址的所有网关账户
+   * @param address 
+   */
+  public async queryAccountsOfGateway(address: string): Promise<object>{
+    return this.get(URLS.v2.gateways.account_all+'/'+address)
   }
 
+  /**
+   * 获取指定用户地址指定币种的所有充值记录
+   * @param address 账户地址
+   * @param currency 币种
+   */
   public async querDepositsToGateway(address: string, currency: string): Promise<object>{
     return this.get(URLS.v2.gateways.deposits+'/'+address)
   }
 
+  /**
+   * 获取指定用户地址指定币种的所有提现记录
+   * @param address 账户地址
+   * @param currency 币种
+   */
   public async querWithdrawalsFromGateway(address: string, currency: string): Promise<object>{
     return this.get(URLS.v2.gateways.withdrawals+'/'+address)
   }
 
+  /**
+   * 获取所有代理人账户
+   * @param limit 数量限制
+   * @param offset 偏移量
+   */
   public async queryAgents(limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.agents.list)
   }
 
+  /**
+   * 获取某个代理下的委托客户
+   * @param agent 代理人名字
+   */
   public async queryClientelesOfAgent(agent: string): Promise<object>{
     return this.get(URLS.v2.agents.clienteles+'/'+agent)
   }
 
+  /**
+   * 获取Group信息
+   * @param address Group地址
+   */
   public async queryGroups(address: string): Promise<object>{
     return this.get(URLS.v2.agents.group+'/'+address)
   }
 
+  /**
+   * 获取所有已注册侧链
+   * @param limit 数量限制
+   * @param offset 偏移量
+   */
   public async querySideChainsRegistered(limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.chains.list)
   }
 
+  /**
+   * 获取全网所有发行商
+   * @param limit 限制结果集个数，最小值：0,最大值：100
+   * @param offset 偏移量，最小值0
+   */
   public async queryIssuers(limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.uia.issuers_list)
   }
 
+  /**
+   * 查询指定发行商的信息
+   * @param address 账户地址
+   */
   public async getIssuerDetail(address: string): Promise<object>{
     return this.get(URLS.v2.uia.issuers_detail+'/'+address)
   }
 
+  /**
+   * 查看指定发行商的资产
+   * @param address 账户地址
+   * @param limit 限制结果集个数，最小值：0,最大值：100
+   * @param offset 偏移量，最小值0
+   */
   public async queryAssetsOfIssuer(address: string, limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.uia.assets_one+'/'+address)
   }
 
+  /**
+   * 获取全网所有资产信息
+   * @param limit 限制结果集个数，最小值：0,最大值：100
+   * @param offset 偏移量，最小值0
+   */
   public async querAllAssets(limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.uia.assets_all)
   }
 
+  /**
+   * 获取指定资产信息
+   * @param currency 资产名
+   */
   public async getAssetDetail(currency: string): Promise<object>{
     return this.get(URLS.v2.uia.assets_detail+'/'+currency)
   }
 
+  /**
+   * 获取指定账户所有uia的余额
+   * @param address 账户地址
+   * @param limit 限制结果集个数，最小值：0,最大值：100
+   * @param offset 偏移量，最小值0
+   */
   public async queryAssetBalances(address: string, limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.uia.balances+'/'+address)
   }
 
+  /**
+   * 查询智能合约列表
+   * @param name 合约名称
+   * @param ownerId 创建人地址
+   * @param address 合约地址
+   * @param limit 限制结果集个数，最小值：0,最大值：100
+   * @param offset 偏移量，最小值0
+   */
   public async queryContracts(name:string='',ownerId:string='', address: string='', limit: number=20, offset: number): Promise<object>{
     return this.get(URLS.v2.contracts.list+'/'+address)
   }
 
+  /**
+   * 查询智能合约详细信息
+   * @param name 合约名称
+   */
   public async getContractDetail(name: string): Promise<object>{
     return this.get(URLS.v2.contracts.detail+'/'+name)
   }
 
+  /**
+   * 查询智能合约代码
+   * @param name 合约名称
+   */
   public async getCodeOfContract(name: string): Promise<object>{
     return this.get(URLS.v2.contracts.code+'/'+name)
   }
 
+  /**
+   * 查询智能合约元数据
+   * @param name 合约名称
+   */
   public async getMetadataOfContract(name: string): Promise<object>{
     return this.get(URLS.v2.contracts.metadata+'/'+name)
   }
 
+  /**
+   *  查询智能合约公开状态
+   * @param name 合约名称
+   * @param path 状态的路径，状态路径是用'.'号分隔的一个字符串，表示要查询的状态所在的合约对象的位置。
+   * 如：
+   * 'amount'表示查询合约的amount属性，
+   * 'payments.0'表示payments对象的第0个元素，
+   * 'paymentOfAddress.ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5'表示合约的paymentOfAddress['ABuH9VHV3cFi9UKzcHXGMPGnSC4QqT2cZ5']。
+   * 以此类推
+   * 
+   * ~~~
+   * 需要注意的是，该方法仅可查询公开的简单属性的值，否则会失败。
+   * 如amount是private的则查询会失败。如paymentOfAddress的类型是Mapping<Payment>这种复杂类型，查询也会失败。
+   * 如需实现更复杂的查询，请参考智能合约开发文档，使用查询方法
+   * ~~~
+   * 
+   */
   public async queryStatesOfContract(name: string, path:string): Promise<object>{
     return this.get(URLS.v2.contracts.states+'/'+name)
   }
 
+  /**
+   * 查询智能合约执行结果
+   * @param name 合约名称
+   * @param tid 执行合约的交易Id
+   */
   public async getResultOfContract(name: string, tid: string): Promise<object>{
     return this.get(URLS.v2.contracts.results+'/'+name)
   }
 
+  /**
+   * 调用查询方法
+   * @param name 合约名称
+   * @param method 查询方法名称
+   * @param args 查询方法参数数组，以json形式放在请求的body中。查询方法参数必须是数组，如果没有参数请使用空数组
+   */
   public async callConstantMethod(name: string, method: string, args: Array<any>): Promise<object>{
     return this.get(URLS.v2.contracts.constants_method+'/'+name)
   }
